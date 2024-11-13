a99822fa3279068d08ba1baa154ab666d595ec53
Mazurek-K <mazurekk21@gmail.com>
2024-11-13 11:18:12 +0100

    works

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,26 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    int towar_sum = 0;
+    int z         = 0;
+    if (towar != 0) {
+
+        while (towar_sum <= towar) {
+
+            Stocznia stocznia{};
+            Statek*  s1 = stocznia();
+
+            int towar_add = s1->transportuj();
+            towar_sum     = towar_sum + towar_add;
+
+            
+            if (Zaglowiec* a = dynamic_cast< Zaglowiec* >(s1)) {
+                z = z + 1; 
+            }
+            
+            delete s1;
+        }
+    }
+
+    return z;
 }
